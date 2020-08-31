e9b8aa6a6c4ef21304f79da75b9d1307d26cbe20
iqbkam18 <kamranali95@hotmail.com>
2020-08-31 10:39:57 +0200

    first commit

---
+++ src/main/java/com/example/BowlingGame/BowlingDemo/BowlingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.BowlingGame.BowlingDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BowlingGame/BowlingDemo/BowlingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BowlingGame.BowlingDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
