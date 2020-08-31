f21bdd9b6c449d25b41bef19b89f044dd5df88e2
iqbkam18 <kamranali95@hotmail.com>
2020-08-31 10:45:08 +0200

    second commit

--- src/main/java/com/example/BowlingGame/BowlingDemo/BowlingDemoApplication.java
+++ src/main/java/com/example/BowlingGame/BowlingDemo/BowlingDemoApplication.java
@@ -3,6 +3,7 @@ package com.example.BowlingGame.BowlingDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Test
 @SpringBootApplication
 public class BowlingDemoApplication {
 
